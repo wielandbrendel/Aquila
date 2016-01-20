3d43729ffb2e7e52a6471befddb9611a6b85d912
wielandbrendel <wieland.brendel@posteo.de>
2016-01-20 22:05:51 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="Aquila",
+    version=0.1,
+    description="A lightweight library to monitor simulations based on Bokeh",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        ],
+    keywords="",
+    author="Wieland Brendel",
+    author_email="wieland.brendel@posteo.de",
+    url="https://github.com/wielandbrendel/Aquila",
+    license="MIT",
+    include_package_data=False,
+    zip_safe=False,
+    )
